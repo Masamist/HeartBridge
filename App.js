b978d6793df590e9695b0f3f92ff2b366d6ea07f
Masamist <msm-k0304@hotmail.com>
2025-10-31 00:43:37 +1300

    add reducer and redux

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import { StyleSheet, useColorScheme, View, Text } from 'react-native';
+import React,{useRef, useEffect} from 'react';
+import {AppState} from "react-native";
+import {View} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
-import MainNavigation from './navigation/MainNavigation';
-
-import Home from './screens/Home/Home';
+import RootNavigation from './navigation/RootNavigation';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
+import {checkToken} from './api/user';
 
 function App() {
-
   return (
     <SafeAreaProvider>
       <AppContent />
@@ -19,6 +23,23 @@ function App() {
 
 function AppContent() {
   const insets  = useSafeAreaInsets();
+
+  const appState = useRef(AppState.currentState)
+  useEffect(() => {
+    const subscription = AppState.addEventListener(
+      'change', 
+      async nextAppState => {
+      if(appState.current.match(/inactive|background/) && 
+        nextAppState === 'active'
+      ){
+        console.log('You are come back in the app')
+        await checkToken();
+      }
+      appState.current = nextAppState
+    })
+    checkToken();
+    console.log('Application has rendered');
+  }, []) 
 
   return (
     <View
@@ -30,9 +51,13 @@ function AppContent() {
         paddingRight: insets.right,
        }} 
       >
-      <NavigationContainer>
-        <MainNavigation />
-      </NavigationContainer>
+        <Provider store={store}>
+          <PersistGate persistor={persistor} loading={null}>
+            <NavigationContainer>
+              <RootNavigation />
+            </NavigationContainer>
+          </PersistGate>
+        </Provider> 
     </View>
   );
 }
--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,25 +1,33 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import { Routes } from './Routes';
-import Home from '../screens/Home/Home';
-import MemberList from '../screens/MemberList.js/MemberList';
+
 import Login from '../screens/Login/Login';
 import Registration from '../screens/Registration/Registration';
+import Home from '../screens/Home/Home';
+import MemberList from '../screens/MemberList.js/MemberList';
 
 const Stack = createStackNavigator();
 
-const  MainNavigation = () => {
+export const NonAuthenticated = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName={Routes.Login}
+      screenOptions={{header: () => null, headerShown: false}}>
+      <Stack.Screen name={Routes.Login} component={Login} />
+      <Stack.Screen name={Routes.Registration} component={Registration} />
+    </Stack.Navigator>
+  );
+};
+
+export const Authenticated = () => {
   return (
     <Stack.Navigator
       initialRouteName={Routes.Login}
       screenOptions={{header: () => null, headerShown: false}}
     >
-      <Stack.Screen name={Routes.Login} component={Login} />
-      <Stack.Screen name={Routes.Registration} component={Registration} />
       <Stack.Screen name={Routes.Home} component={Home} />
       <Stack.Screen name={Routes.MemberList} component={MemberList} />
     </Stack.Navigator>
 
   );
-}
-
-export default MainNavigation;
+};
